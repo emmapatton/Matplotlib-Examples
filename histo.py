63f71b7327cbd6b27a91d12e64201265c57140c7
emmapatton <G00364733@gmit.ie>
2018-10-14 17:12:18 +0100

    Plotting histogram side by side

--- histo.py
+++ histo.py
@@ -4,9 +4,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.random.normal(0.0, 1.0, 1000)
+plt.subplot(1, 2, 1)
+x = np.random.normal(0.0, 1.0, 10000)
 
-plt.hist(x, bins=20)
+plt.hist(x)
+
+plt.subplot(1, 2, 2)
+x = np.random.uniform(-3.0, 3.0, 10000)
+
+plt.hist(x)
 
 plt.show()
 
