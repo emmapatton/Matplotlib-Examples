84681b86cc7a7f2bfa1ac44ae7a189dd187f1bf3
emmapatton <G00364733@gmit.ie>
2018-10-14 16:11:37 +0100

    Creating two plots together

--- 2dplot.py
+++ 2dplot.py
@@ -1,9 +1,14 @@
 #Emma Patton, 2018-10-07
-# 2D Plot Practice
+#2D Plot Practice - Scatter Plots 
 
 import matplotlib.pyplot as plt 
+import numpy as np 
 
-plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
-plt.ylabel("Some Numbers")
-plt.xlabel("None")
+x = np.arange(0.0, 10.0, 0.01)
+y = 3.0 * x + 1.0
+noise = np.random.normal(0.0, 1.0, len(x))
+
+plt.plot(x, y + noise, "r.")
+plt.plot(x, y, "b-")
+
 plt.show()
